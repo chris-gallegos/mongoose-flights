c5e8b4d0e3f67d2f4f14e468cd9cdb78a688dc6d
chris-gallegos <chrisgallegos101@gmail.com>
2022-10-01 16:03:26 -0700

    added ascending order feature and display of default departure date

--- controllers/flights.js
+++ controllers/flights.js
@@ -7,14 +7,18 @@ module.exports = {
 }
 
 function index(req, res) {
-    Flight.find({}, function(err, flights) {
+    Flight.find({}).sort({departs: 1}).exec(function(err, flights) {
         console.log(flights)
         res.render('flights/index', {flights})
     })
 }
 
 function newFlight(req, res) {
-    res.render('flights/new')
+    const newFlight = new Flight()
+    const dt = newFlight.departs
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`
+    res.render('flights/new', { departsDate });
 }
 
 function create(req, res) {
